e9deb64504974a9f4aebc32e8fbcf30062600230
mhtkmr1 <mohitsept4@gmail.com>
2021-11-21 16:50:45 +0530

    Update safety_chatbot_webapp.py

--- safety_chatbot_webapp.py
+++ safety_chatbot_webapp.py
@@ -1,8 +1,9 @@
+# streamlit4.py
 import streamlit as st
 import pandas as pd
 import numpy as np
 import time
-#import joblib
+# import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import nltk                                         #Natural language processing tool-kit
 from nltk.corpus import stopwords                   #Stopwords corpus
@@ -340,9 +341,9 @@ def preprecess_chatbot(sentence,vector):
   return output
 
 # @st.cache(suppress_st_warning=True,allow_output_mutation=True)
-def load_model():
+#def load_model():
   #final_model = joblib.load('final_model.pkl')
-  return final_model
+  #return final_model
 
 #st.write("_" * 30)
 # >>>>>>>>>>>>>>>>
@@ -588,7 +589,8 @@ def main():
                       with col101:
                         run_chatbot = st.button(label = 'Load model and Run the chatbot',key = 'run_chatbot')
                       if run_chatbot:
-			st.session_state.final_model=st.session_state.current_model # load_model()
+                          st.session_state.final_model = st.session_state.current_model #load_model()
+
                       if 'run_chatbot' in st.session_state:
                         with col102:
                           description = st.text_area('Please enter the Accident description here', value='default_value',
